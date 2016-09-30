54ece16ea610f966ed5c8a8e32f3e01049013a76
Ottony Costa <ottony93@gmail.com>
2016-09-29 21:51:56 -0300

    Initial Commit

---
+++ dist/index.js
@@ -0,0 +1,36 @@
+const _ = require('underscore');
+const base = 255;
+
+const createKey = date => {
+  return date.replace(/[\/-]/g, '').split('');
+};
+
+const transform = (raw, date, pass, encoding) => {
+  const buffer = Buffer.from(raw, encoding);
+  const key = createKey(date);
+  const keyLength = _.size(key);
+
+  let step, transformed;
+
+  return new Buffer(_.map(buffer, (c, i) => {
+    step = pass * key[i % keyLength];
+    transformed = c + step;
+
+    return (base + transformed % base) % base;
+  }));
+};
+
+const cypher = (raw, date, encoding) => {
+  return transform(raw, date, 1, encoding);
+};
+
+const decipher = (raw, date, encoding) => {
+  return transform(raw, date, -1, encoding);
+};
+
+const DateCypher = {
+  cypher: cypher,
+  decipher: decipher
+};
+
+module.exports = DateCypher;
